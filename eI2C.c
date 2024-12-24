0648d081753c2f4f6ceab1b34cba4991e6af6d5d
Esteban <acevedoesteban999@gmail.com>
2024-12-24 01:09:09 -0500

    UPD: CHECK i2c Connection Error

--- eI2C.c
+++ eI2C.c
@@ -1,5 +1,12 @@
 #include "eI2C.h"
 
+int error_ei2c = -1;
+
+
+bool ei2c_has_error(){
+    return error_ei2c != 0;
+}
+
 esp_err_t ei2c_master_init(int SDA, int SCL, int I2C_Port)
 {
     i2c_config_t conf = {
@@ -12,16 +19,15 @@ esp_err_t ei2c_master_init(int SDA, int SCL, int I2C_Port)
     };
 
     esp_err_t err = i2c_param_config(I2C_Port, &conf);
-    if (err != ESP_OK) {
+    if (err != ESP_OK)
         ESP_LOGE("", "I2C param config failed: %s", esp_err_to_name(err));
-        return err;
+    else{
+        err = i2c_driver_install(I2C_Port, conf.mode, 0, 0, 0);
+        if (err != ESP_OK)
+            ESP_LOGE("", "I2C driver install failed: %s", esp_err_to_name(err));
     }
 
-    err = i2c_driver_install(I2C_Port, conf.mode, 0, 0, 0);
-    if (err != ESP_OK) {
-        ESP_LOGE("", "I2C driver install failed: %s", esp_err_to_name(err));
-    }
-
+    error_ei2c = err;
     return err;
 }
 
@@ -63,12 +69,15 @@ void ei2c_scan(int SDA, int SCL, int I2C_Port){
 }
 
 esp_err_t ei2c_write(i2c_port_t I2C_PORT, uint8_t ADDRESS ,uint8_t data_t ,unsigned len){
-    esp_err_t err;
-    for (int i = 0; i < MAX_TRY; i++) {
-        err = i2c_master_write_to_device(I2C_PORT, ADDRESS, data_t, 4, MAX_TIKS_WAIT);
-        if (err == ESP_OK) break;
-        vTaskDelay(pdMS_TO_TICKS(MAX_DELAY_TRY)); 
+    if(!ei2c_has_error()){
+        esp_err_t err;
+        for (int i = 0; i < MAX_TRY; i++) {
+            err = i2c_master_write_to_device(I2C_PORT, ADDRESS, data_t, 4, MAX_TIKS_WAIT);
+            if (err == ESP_OK) break;
+            vTaskDelay(pdMS_TO_TICKS(MAX_DELAY_TRY)); 
+        }
+        vTaskDelay(pdMS_TO_TICKS(2)); 
+        return err;
     }
-    vTaskDelay(pdMS_TO_TICKS(2)); 
-    return err;
+    return error_ei2c;
 }
